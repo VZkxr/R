0d3286fa9ca9c58a60e37f9b09e4d960e0b1c8ef
Aarón García <plaxzyt@gmail.com>
2024-09-01 17:58:54 -0600

    Columns

--- Ejercicios/ejercicio_de_clase.R
+++ Ejercicios/ejercicio_de_clase.R
@@ -5,16 +5,36 @@ gc()
 library(readxl)
 
 ## Pull data
-Data <- read_excel(path = "Concentrado_indicadores_de_pobreza_2020.xlsx",
+Data <- read_excel(path = "C:/Users/Aaron/Documents/R/Ejercicios/Concentrado_indicadores_de_pobreza_2020.xlsx",
                     sheet = "Concentrado estatal", #Permite especificar la hoja
-                    range = cell_cols(c("C:N")), #Rango de celdas
-                    col_names = LETTERS[3:14])
+                    range = cell_cols(c("B:DD")), #Rango de celdas
+                    )
 
 
-## Select columns "C", "E", "K", "N"
-Data <- Data[,c("C", "E", "K", "N")]
+## Select columns
+Data <- Data[,c(2, 101, 104)]
 
 ## Change columns' names 
-colnames(Datos) <- c("Entidad", "Municipio", "Porcentaje2020", "Personas2020")
+colnames(Data) <- c("Entidad", "Porcentaje2020", "Personas2020")
 
 str(Data)
+
+## Drop first 4 columns
+Data <- Data[-c(1:4),]
+
+## Convert character to factor and numeric type
+Data$Entidad <- factor(Data$Entidad)
+Data$Porcentaje2020 <- is.numeric(Data$Porcentaje2020)
+Data$Personas2020 <- is.numeric(Data$Personas2020)
+
+##Verify and delete NA
+Data[is.na(Data$Porcentaje2020),]
+Data <- Data[!is.na(Data$Porcentaje2020),]
+
+str(Data)
+
+
+
+
+
+
